74e2b37317e1fc426fee96394cfebd89a377188a
mohamedMounir <mohammedmonir2254@gmail.com>
2020-12-11 15:10:45 +0200

    changes that i did.

--- fashionShop/fashionShop.Android/MainActivity.cs
+++ fashionShop/fashionShop.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using CarouselView.FormsPlugin.Android;
 
 namespace fashionShop.Droid
 {
@@ -20,6 +21,7 @@ namespace fashionShop.Droid
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            CarouselViewRenderer.Init();
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
